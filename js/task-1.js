da17f7f3ccbc146f3a6560ceaf761519224261a0
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2024-09-29 17:50:18 +0300

    goit-js-hw-07

--- js/task-1.js
+++ js/task-1.js
@@ -1,32 +1,10 @@
-"use strict";
+const numberOfCategories = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${numberOfCategories.length}`);
 
-const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    // Change code below this line
-    getBalance() {
-        return this.balance;
-    },
-    getDiscount() {
-        return this.discount;
-    },
-    setDiscount(value) {
-        this.discount = value;
-    },
-    getOrders() {
-        return this.orders;
-    },
-    addOrder(cost, order) {
-        this.balance -= cost - cost * this.discount;
-        this.orders.push(order);
-    },
-    // Change code above this line
-};
+for (const item of numberOfCategories) {
+    const title = item.querySelector("h2");
+    const list = item.querySelectorAll("li");
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+    console.log(`Category: ${title.textContent}`);
+    console.log(`Elements: ${list.length}`);
+}
--- js/task-3.js
+++ js/task-3.js
@@ -1,37 +1,10 @@
-"use strict";
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
 
-class StringBuilder {
-    #value;
-
-    constructor(initialValue) {
-        this.#value = initialValue;
+textInput.addEventListener("input", (event) => {
+    if (textInput.value.trim()) {
+        textOutput.textContent = textInput.value.trim();
+    } else {
+        return (textOutput.textContent = "Anonymouse");
     }
-
-    getValue() {
-        return this.#value;
-    }
-
-    padStart(str) {
-        this.#value = this.#value.padStart(this.#value.length + 1, str);
-    }
-
-    padEnd(str) {
-        this.#value = this.#value.padEnd(this.#value.length + 1, str);
-    }
-
-    padBoth(str) {
-        this.#value = this.#value
-            .padStart(this.#value.length + 1, str)
-            .padEnd(this.#value.length + 2, str);
-    }
-}
-
-const builder = new StringBuilder(".");
-
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+});
